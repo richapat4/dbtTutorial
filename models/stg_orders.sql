25c099e57206470f5a819443c1c07fed2c398fde
rpatel2@deloitte.com.au <rpatel2@deloitte.com.au>
2023-01-18 03:01:19 +0000

    Learned how to write a test file, and break the test file to see if it works

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,20 +1,4 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+select id as order_id, user_id as customer_id, order_date, status
+
 
 from `dbt-tutorial`.jaffle_shop.orders
-
-
--- {{ config(
---    post_hook=“UPDATE {{ this }} SET column_name=‘value’”
--- ) }}
-
-
-
--- #standardSQL
--- UPDATE `dbt-tutorial`.jaffle_shop.customers
--- SET status  = 'rubbish'
--- where order_id = 84;
-
