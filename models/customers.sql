4654cc2c63ce9d1b20dd57a7a603708c5ac157e2
rpatel2@deloitte.com.au <rpatel2@deloitte.com.au>
2023-01-18 02:56:42 +0000

    Testing dbt

--- models/customers.sql
+++ models/customers.sql
@@ -7,26 +7,38 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
+
+-- with customers as (
+
+--     select
+--         id as customer_id,
+--         first_name,
+--         last_name
+
+--     from `dbt-tutorial`.jaffle_shop.customers
+
+-- ),
+
+-- orders as (
+
+--     select
+--         id as order_id,
+--         user_id as customer_id,
+--         order_date,
+--         status
+
+--     from `dbt-tutorial`.jaffle_shop.orders
+
+-- ),
 
 customer_orders as (
 
